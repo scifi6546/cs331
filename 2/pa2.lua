dad841d5ec4a2db227a2171100980fcbb1c8953b
BuildTools <unconfigured@null.spigotmc.org>
2020-02-11 19:43:40 -0900

    commented code

--- 2/pa2.lua
+++ 2/pa2.lua
@@ -1,5 +1,10 @@
+--- Nicholas ALexeev
+--- 02/11/20
+--- pa2.lua
+--- purpose: completes assignment 2
 local pa={}
 
+--- filteres elements in table by fn
 function pa.filterTable(fn,tbl)
 	local tbl_out={}
 	for k,v in pairs(tbl)
@@ -13,11 +18,13 @@ function pa.filterTable(fn,tbl)
 	end
 	return tbl_out
 end
+--- appens maximum copies of str
 function pa.concatMax(str,end_len)
 	local num_concat = math.floor(end_len/str:len(str))
 	return str:rep(num_concat)
 	
 end
+--- coroutine that returns numbers in collatz conjecture
 function pa.collatz(n)
 	while(n	~=1) do
 		if(n%2==0) then
@@ -34,6 +41,7 @@ function pa.collatz(n)
 	coroutine.yield(n);
 
 end
+--- returns iterator to all substrings of str
 function pa.allSubs(str)
 	local current_len =0 
 	local index =0 
