6a982c42fa8120478c50b8248c1fae046cacd229
BuildTools <nickalexeev@protonmail.com>
2020-04-02 14:41:21 -0800

    got median working

--- 5/median.hs
+++ 5/median.hs
@@ -1,9 +1,20 @@
-getArr array
-  | str==return("\n") = array
-  | otherwise = str --fmap (read) (str) : array
-  where a = str >>= getLine
+import Data.List (sort)
+getArr arr = do
+          item <- getLine
+          if item == ""
+              then return arr
+              else getArr (( read item ::Integer) : arr)
+	          
+get_median arr
+   | len `mod` 2 == 0 = ((arr_sorted !! (index-1)) + (arr_sorted !! (index))) `div` 2
+   | len `mod` 2 == 1 = arr_sorted !! ((len `div` 2))
+   where
+     arr_sorted = sort arr
+     len = length arr
+     index = len `div` 2
 main :: IO()
-main = do putStrLn "Please Input Numbers followed by newline\nWarning Inputting a non int may crash the program as no error handeling occurs"
-          c <- getLine
-          let i = read c :: Integer
-          putStrLn (show i)
+main = do putStrLn "Please Input Numbers followed by newline\nWarning Inputting a non int may crash the program as no error handeling occurs\nOutputs floored median"
+	  a <- getArr []
+	  let med = get_median a
+	  putStrLn "Median"
+          putStrLn (show med)
